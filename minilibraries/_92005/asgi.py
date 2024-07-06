731202b80048eab51883096974ca64220b33a788
Logan B <logangbentley@gmail.com>
2024-07-06 16:54:57 +1200

    Various changes needed to get ML to host

--- minilibraries/_92005/asgi.py
+++ minilibraries/_92005/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', '92005.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '_92005.settings')
 
 application = get_asgi_application()
