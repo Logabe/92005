93564e9dd063f3ca553be5a77a7241fcdc75b206
Logan B <logangbentley@gmail.com>
2024-07-08 15:43:48 +1200

    Removed length limitation from Book Registration form as it was too short

--- minilibraries/minilibraries/forms.py
+++ minilibraries/minilibraries/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class RegisterBookForm(forms.Form):
-    isbn = forms.CharField(label="ISBN", max_length=13, required=True)
+    isbn = forms.CharField(label="ISBN", required=True)
